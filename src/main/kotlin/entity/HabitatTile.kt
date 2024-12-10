bf4afacb2b0b70e41fa55ac90c6c18d6da4189e2
Murat <murat.aslan@tu-dortmund.de>
2024-12-10 13:40:16 +0100

    add HabitatTile

--- src/main/kotlin/entity/HabitatTile.kt
+++ src/main/kotlin/entity/HabitatTile.kt
@@ -3,6 +3,7 @@ package entity
 class HabitatTile(
     val id: Int,
     var isKeystoneTile: Boolean,
+    var rotationOffset: Int, //Integer between 0 and 5
     /****/
     val wildlifeSymbols: Animal,
     var wildlifeToken: WildlifeToken?,
