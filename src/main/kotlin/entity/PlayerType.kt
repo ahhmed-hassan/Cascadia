b82031667b4c0d2703cdc9f6775d0ad80e5df6cf
Murat <murat.aslan@tu-dortmund.de>
2024-12-10 19:19:42 +0100

    Add PlayerType

--- src/main/kotlin/entity/PlayerType.kt
+++ src/main/kotlin/entity/PlayerType.kt
@@ -4,14 +4,15 @@ package entity
  * Enum [PlayerType] representing the different types of players in the Cascadia game.
  * This classification determines the player's mode of interaction with the game.
  *
- * @property LOCAL Represents a local player controlling the game directly on the device.
- * @property EASY Represents an AI player with an easy difficulty level.
- * @property NORMAL Represents an AI player with a normal difficulty level.
- * @property NETWORK Represents a player connected through a network for multiplayer gameplay.
+ * LOCAL, Represents a local player controlling the game directly on the device.
+ *  EASY, Represents an AI player with an easy difficulty level.
+ *  NORMAL, Represents an AI player with a normal difficulty level.
+ * NETWORK, Represents a player connected through a network for multiplayer gameplay.
  */
 enum class PlayerType {
     LOCAL,
     EASY,
     NORMAL,
-    NETWORK
+    NETWORK,
+    ;
 }
