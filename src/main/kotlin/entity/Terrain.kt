3e3d6f642076cb7b3f255450ac0c5deeb21e65d2
Murat <murat.aslan@tu-dortmund.de>
2024-12-10 17:08:29 +0100

    Add Terrain

---
+++ src/main/kotlin/entity/Terrain.kt
@@ -0,0 +1,19 @@
+package entity
+
+/**
+ * Enum [Terrain] representing the different terrain types in the Cascadia game.
+ * Each terrain type is associated with specific habitat tiles.
+ *
+ * @property FOREST Represents a forest terrain.
+ * @property MOUNTAIN Represents a mountain terrain.
+ * @property PRAIRIE Represents a prairie terrain.
+ * @property RIVER Represents a river terrain.
+ * @property WETLAND Represents a wetland terrain.
+ */
+enum class Terrain {
+    FOREST,
+    MOUNTAIN,
+    PRAIRIE,
+    RIVER,
+    WETLAND;
+}
