c3f388bffad0aef0def495cee6c4893aea2d636a
Murat <murat.aslan@tu-dortmund.de>
2024-12-10 19:19:01 +0100

    Add Animal

--- src/main/kotlin/entity/Animal.kt
+++ src/main/kotlin/entity/Animal.kt
@@ -4,16 +4,17 @@ package entity
  * Enum [Animal] representing the types of animals found in the Cascadia game.
  * Each animal has specific behaviors and scoring rules defined in the game.
  *
- * @property BEAR Represents a bear in the game.
- * @property ELK Represents an elk in the game.
- * @property FOX Represents a fox in the game.
- * @property HAWK Represents a hawk in the game.
- * @property SALMON Represents a salmon in the game.
+ * BEAR, Represents a bear in the game.
+ * ELK, Represents an elk in the game.
+ * FOX, Represents a fox in the game.
+ * HAWK, Represents a hawk in the game.
+ * SALMON, Represents a salmon in the game.
  */
 enum class Animal {
     BEAR,
     ELK,
     FOX,
     HAWK,
-    SALMON;
+    SALMON,
+    ;
 }
