7ace1c2d9b02679208046f22b88b07e810ae19db
Murat <murat.aslan@tu-dortmund.de>
2024-12-10 17:10:39 +0100

    Add WildlifeToken

---
+++ src/main/kotlin/entity/WildlifeToken.kt
@@ -0,0 +1,12 @@
+package entity
+
+/**
+ * [WildlifeToken] Represents a wildlife token in the Cascadia game.
+ * Wildlife tokens are associated with specific animals and can be placed on habitat tiles.
+ *
+ * @property animal The animal type represented by this token (e.g., Bear, Elk).
+ */
+class WildlifeToken(
+    val animal: Animal
+) {
+}
