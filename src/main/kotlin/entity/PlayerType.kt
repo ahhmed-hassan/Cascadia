45b33ee4a6eb86f55bd76b07a4ca1de44678fb63
Murat <murat.aslan@tu-dortmund.de>
2024-12-12 12:01:21 +0100

    add PlayerType

--- src/main/kotlin/entity/PlayerType.kt
+++ src/main/kotlin/entity/PlayerType.kt
@@ -5,8 +5,8 @@ package entity
  * This classification determines the player's mode of interaction with the game.
  *
  * LOCAL, Represents a local player controlling the game directly on the device.
- *  EASY, Represents an AI player with an easy difficulty level.
- *  NORMAL, Represents an AI player with a normal difficulty level.
+ * EASY, Represents an AI player with an easy difficulty level.
+ * NORMAL, Represents an AI player with a normal difficulty level.
  * NETWORK, Represents a player connected through a network for multiplayer gameplay.
  */
 enum class PlayerType {
