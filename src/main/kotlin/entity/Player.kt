77148ab92534292500fd5133d4770d16a1d5344d
Mertcan <Mertcan.Aydin@tu-dortmund.de>
2024-12-12 12:43:15 +0100

    added suggestions from the comments

--- src/main/kotlin/entity/Player.kt
+++ src/main/kotlin/entity/Player.kt
@@ -7,12 +7,10 @@ package entity
  * @param habitat A map representing the player's placed habitat tiles and wildlife tokens.
  * @param playerType The type of the player, which signalises if the player is human or a bot.
  *
- * @property hasPlayedTile Indicates whether the player has already played a tile in the current turn.
  * @property natureToken The number of nature tokens the player currently holds.
  * @property score The player's current score.
  */
-class Player(name: String, habitat: Map<Pair<Int, Int>, HabitatTile>, playerType: PlayerType) {
-    var hasPlayedTile : Boolean = false
+class Player(val name: String, val habitat: Map<Pair<Int, Int>, HabitatTile>, val playerType: PlayerType) {
     var natureToken : Int = 0
     var score : Int = 0
 }
