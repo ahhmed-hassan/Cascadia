4dab7cc3e141b7cefa00cd1660f138caf2a2111f
ahmed2.hassan <ahmed2.hassan@tu-dortmund.de>
2024-12-15 00:31:55 +0100

    Adding getNeighbours utility lambda

--- src/main/kotlin/service/ScoringService.kt
+++ src/main/kotlin/service/ScoringService.kt
@@ -32,6 +32,9 @@ class ScoringService(private val rootSerivce : RootService) : AbstractRefreshing
         private val addPairs: (Pair<Int, Int>, Pair<Int, Int>) -> Pair<Int, Int> = { a, b ->
             a.first + b.first to a.second + b.second
         }
+        private val getNeighbours : (Pair<Int,Int>) -> List<Pair<Int,Int>> = {
+            pair -> directionsPairsAndCorrespondingEdges.keys.map { addPairs(pair, it) }
+        }
 
         /**
          * Calculating the longest path starting at some coordinates
