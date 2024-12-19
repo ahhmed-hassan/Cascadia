3e6355ddffc4e1f0a055ea7a64a60cbf67b0eb6f
ahmed2.hassan <ahmed2.hassan@tu-dortmund.de>
2024-12-19 22:28:41 +0100

    Handling the two players case

--- src/main/kotlin/service/ScoringService.kt
+++ src/main/kotlin/service/ScoringService.kt
@@ -1,5 +1,6 @@
 package service
 
+import entity.Animal
 import entity.Terrain
 import entity.Player
 
@@ -9,7 +10,7 @@ import entity.Player
  *
  *  @param [rootService] the games RootService for communication with entity layer
  */
-class ScoringService(private val rootService : RootService) : AbstractRefreshingService() {
+class ScoringService(private val rootService: RootService) : AbstractRefreshingService() {
 
 
     companion object {
@@ -32,11 +33,30 @@ class ScoringService(private val rootService : RootService) : AbstractRefreshing
         private val addPairs: (Pair<Int, Int>, Pair<Int, Int>) -> Pair<Int, Int> = { a, b ->
             a.first + b.first to a.second + b.second
         }
-        private val getNeighbours : (Pair<Int,Int>) -> List<Pair<Int,Int>> = {
-            pair -> directionsPairsAndCorrespondingEdges.keys.map { addPairs(pair, it) }
+        private val getNeighbours: (Pair<Int, Int>) -> List<Pair<Int, Int>> = { pair ->
+            directionsPairsAndCorrespondingEdges.keys.map { addPairs(pair, it) }
         }
-        private fun Pair<Int,Int>.neighbours () : List<Pair<Int,Int>>{
-            return directionsPairsAndCorrespondingEdges.keys.map { addPairs(it,this) }
+
+        private fun Pair<Int, Int>.neighbours(): List<Pair<Int, Int>> {
+            return directionsPairsAndCorrespondingEdges.keys.map { addPairs(it, this) }
+        }
+
+        /**
+         * A class aggregating all the detailed bonus points
+         * @property animalsScores a Map for Animal, Score pairs for each animal
+         * @property ownLongestTerrainsScores a map for the longest connected [Terrain]s for each one
+         * @property natureTokens the number of nature tokens left for the player
+         * @property longestAmongOtherPlayers the bonus points the player becomes when having the longest Terrains
+         */
+        data class PlayerScore(
+            val animalsScores: Map<Animal, Int>,
+            val ownLongestTerrainsScores: Map<Terrain, Int>,
+            val natureTokens: Int = 0,
+            var longestAmongOtherPlayers: Map<Terrain, Int> =
+                Terrain.values().associateWith { 0 }.toMutableMap()
+        ){
+            val sum : () -> Int = {animalsScores.values.sum() + ownLongestTerrainsScores.values.sum() +
+                    longestAmongOtherPlayers.values.sum() + natureTokens}
         }
 
         /**
@@ -45,26 +65,80 @@ class ScoringService(private val rootService : RootService) : AbstractRefreshing
          * @param graph the graph to search
          * @param visited the visited coordinates so far
          */
-        private fun depthFirstConnectedComponentLength(graph: Map<Pair<Int,Int>, List<Pair<Int,Int>>>,
-                                            visited : MutableSet<Pair<Int,Int>>,
-                                            coordinate: Pair<Int, Int>) : Int {
-            if(visited.contains(coordinate)) return 0
-            var connectedComponentLength : Int = 1
+        private fun depthFirstConnectedComponentLength(
+            graph: Map<Pair<Int, Int>, List<Pair<Int, Int>>>,
+            visited: MutableSet<Pair<Int, Int>>,
+            coordinate: Pair<Int, Int>
+        ): Int {
+            if (visited.contains(coordinate)) return 0
+            var connectedComponentLength: Int = 1
             visited.add(coordinate)
             val neighbours = coordinate.neighbours()
 
-            for(neighbour in neighbours ){
-                if(!visited.contains(neighbour))
-                    connectedComponentLength+= depthFirstConnectedComponentLength(graph, visited, neighbour)
+            for (neighbour in neighbours) {
+                if (!visited.contains(neighbour))
+                    connectedComponentLength += depthFirstConnectedComponentLength(graph, visited, neighbour)
             }
             return connectedComponentLength
 
         }
     }
+
+    /**
+     * TODO
+     */
+    fun calculateScoresForThreeORMorePlayers(): Map<Player, PlayerScore> {
+        //TODO
+        val game = checkNotNull(rootService.currentGame) { "No game started yet" }
+        return emptyMap()
+    }
+
+    /**
+     *Calculates the detailed scores for every player in the game
+     * preconditions : The game has only two players
+     * returns a map from each player to the [PlayerScore] object having all the infos for the scoring boards
+     */
+    fun calculateScoresForTwoPlayers(): Map<Player, PlayerScore> {
+        val game = checkNotNull(rootService.currentGame) { "No game started yet" }
+        check(game.playerList.size==2) {"Only call this when having only two players"}
+
+        val playersScores = game.playerList.associateWith { player ->
+            PlayerScore(animalsScores = mapOf(
+                Animal.BEAR to calculateBearScore(player),
+                Animal.SALMON to calculateSalmonScore(player),
+                Animal.ELK to calculateElkScore(player),
+                Animal.FOX to calculateFoxScore(player),
+                Animal.HAWK to calculateHawkScore(player)
+            )
+                , ownLongestTerrainsScores = Terrain.values().associateWith { calculateLongestTerrain(it, player) }
+                , natureTokens = player.natureToken)
+        }
+
+        val (firstPlayer, secondPlayer) = playersScores.keys.toList()
+        val firstPlayerLongestTerrains = playersScores[firstPlayer]?.ownLongestTerrainsScores ?: emptyMap()
+        val secondPlayerLongestTerrains = playersScores[secondPlayer]?.ownLongestTerrainsScores ?: emptyMap()
+
+        val firstPlayerBonusMap = Terrain.values().associateWith {
+            if (firstPlayerLongestTerrains[it]!! > secondPlayerLongestTerrains[it]!!) 2
+            else 0
+        }
+        val secondPlayerBonusMap = Terrain.values().associateWith {
+            if (secondPlayerLongestTerrains[it]!! > firstPlayerLongestTerrains[it]!!) 2
+            else 0
+        }
+
+        return playersScores.toMutableMap().apply {
+            this[firstPlayer] = this[firstPlayer]!!.copy(longestAmongOtherPlayers = firstPlayerBonusMap)
+            this[secondPlayer] = this[secondPlayer]!!.copy(longestAmongOtherPlayers = secondPlayerBonusMap)
+        }
+
+    }
+
+
     /**
      *
      */
-    fun calculateScore(player : Player): Int {
+    fun calculateScore(player: Player): Int {
         //ToDo
 
         onAllRefreshables { /*ToDo*/ }
@@ -74,7 +148,7 @@ class ScoringService(private val rootService : RootService) : AbstractRefreshing
     /**
      *
      */
-    private fun calculateLongestTerrain(type : Terrain, player : Player):Int {
+    private fun calculateLongestTerrain(type: Terrain, player: Player): Int {
         //ToDo
         return 0
     }
@@ -82,7 +156,7 @@ class ScoringService(private val rootService : RootService) : AbstractRefreshing
     /**
      *
      */
-    private fun calculateBearScore(player : Player): Int {
+    private fun calculateBearScore(player: Player): Int {
         //ToDo
         return 0
     }
@@ -90,7 +164,7 @@ class ScoringService(private val rootService : RootService) : AbstractRefreshing
     /**
      *
      */
-    private fun calculateElkScore(player : Player): Int {
+    private fun calculateElkScore(player: Player): Int {
         //ToDo
         return 0
     }
@@ -98,7 +172,7 @@ class ScoringService(private val rootService : RootService) : AbstractRefreshing
     /**
      *
      */
-    private fun calculateHawkScore(player : Player) : Int {
+    private fun calculateHawkScore(player: Player): Int {
         //ToDo
         return 0
     }
@@ -106,7 +180,7 @@ class ScoringService(private val rootService : RootService) : AbstractRefreshing
     /**
      *
      */
-    private fun calculateSalmonScore(player : Player): Int {
+    private fun calculateSalmonScore(player: Player): Int {
         //ToDo
         return 0
     }
@@ -114,7 +188,7 @@ class ScoringService(private val rootService : RootService) : AbstractRefreshing
     /**
      *
      */
-    private fun calculateFoxScore(player : Player): Int {
+    private fun calculateFoxScore(player: Player): Int {
         //ToDo
         return 0
     }
