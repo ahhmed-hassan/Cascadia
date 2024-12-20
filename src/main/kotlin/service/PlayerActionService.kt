e479cbb53a5ade7170292fd8344c8c778fa7ea2d
Mertcan <Mertcan.Aydin@tu-dortmund.de>
2024-12-20 13:31:36 +0100

    code was fixed based on reviews

--- src/main/kotlin/service/PlayerActionService.kt
+++ src/main/kotlin/service/PlayerActionService.kt
@@ -11,17 +11,26 @@ import entity.HabitatTile
 class PlayerActionService(private val rootService : RootService) : AbstractRefreshingService() {
 
     /**
-     * [chooseTokenTilePair] is responsible for marking the chosen Token-Tile Pair as selected for further use.
+     * [chooseTokenTilePair] This function is responsible for saving a Token-Tile pair as "selected" within the game.
+     * Additionally, after a pair is selected, it is replaced in the shop with a placeholder pair of null values
      *
-     * @param chosenPair This the pair that the player chose from the shop.
+     * @param chosenPair These are the indices of the pair that the player chose from the shop.
+     *
+     * @throws IllegalArgumentException if the chosenPair is out of bounds
      */
     fun chooseTokenTilePair(chosenPair : Int) {
         val game = rootService.currentGame
         checkNotNull(game)
+
+        // check if chosenPair is not out of bounds
+        require(chosenPair in 0..3) {"Index for pair must be between 0 and 3"}
+
         val shopTile = game.shop[chosenPair].first
         val shopToken = game.shop[chosenPair].second
         checkNotNull(shopTile)
         checkNotNull(shopToken)
+
+        require(game.selectedTile == null && game.selectedToken == null)
 
         //mark the chosen Token-Tile Pair as selected
         game.selectedTile = shopTile
