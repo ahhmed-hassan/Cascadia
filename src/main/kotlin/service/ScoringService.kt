a024e2b2f77aefafb668e0fa6f9d419c145dd7ae
ahmed2.hassan <ahmed2.hassan@tu-dortmund.de>
2024-12-22 02:01:58 +0100

    Code documenting

--- src/main/kotlin/service/ScoringService.kt
+++ src/main/kotlin/service/ScoringService.kt
@@ -2,8 +2,8 @@ package service
 
 import entity.Animal
 import entity.HabitatTile
-import entity.Terrain
 import entity.Player
+import entity.Terrain
 
 
 /**
@@ -11,7 +11,7 @@ import entity.Player
  *
  *  @param [rootService] the games RootService for communication with entity layer
  */
-class ScoringService(private val rootService : RootService) : AbstractRefreshingService() {
+class ScoringService(private val rootService: RootService) : AbstractRefreshingService() {
 
 
     companion object {
@@ -34,11 +34,12 @@ class ScoringService(private val rootService : RootService) : AbstractRefreshing
         private val addPairs: (Pair<Int, Int>, Pair<Int, Int>) -> Pair<Int, Int> = { a, b ->
             a.first + b.first to a.second + b.second
         }
-        private val getNeighbours : (Pair<Int,Int>) -> List<Pair<Int,Int>> = {
-            pair -> directionsPairsAndCorrespondingEdges.keys.map { addPairs(pair, it) }
+        private val getNeighbours: (Pair<Int, Int>) -> List<Pair<Int, Int>> = { pair ->
+            directionsPairsAndCorrespondingEdges.keys.map { addPairs(pair, it) }
         }
-        private fun Pair<Int,Int>.neighbours () : List<Pair<Int,Int>>{
-            return directionsPairsAndCorrespondingEdges.keys.map { addPairs(it,this) }
+
+        private fun Pair<Int, Int>.neighbours(): List<Pair<Int, Int>> {
+            return directionsPairsAndCorrespondingEdges.keys.map { addPairs(it, this) }
         }
 
         /**
@@ -47,26 +48,29 @@ class ScoringService(private val rootService : RootService) : AbstractRefreshing
          * @param graph the graph to search
          * @param visited the visited coordinates so far
          */
-        private fun depthFirstConnectedComponentLength(graph: Map<Pair<Int,Int>, List<Pair<Int,Int>>>,
-                                            visited : MutableSet<Pair<Int,Int>>,
-                                            coordinate: Pair<Int, Int>) : Int {
-            if(visited.contains(coordinate)) return 0
-            var connectedComponentLength : Int = 1
+        private fun depthFirstConnectedComponentLength(
+            graph: Map<Pair<Int, Int>, List<Pair<Int, Int>>>,
+            visited: MutableSet<Pair<Int, Int>>,
+            coordinate: Pair<Int, Int>
+        ): Int {
+            if (visited.contains(coordinate)) return 0
+            var connectedComponentLength: Int = 1
             visited.add(coordinate)
             val neighbours = coordinate.neighbours()
 
-            for(neighbour in neighbours ){
-                if(!visited.contains(neighbour))
-                    connectedComponentLength+= depthFirstConnectedComponentLength(graph, visited, neighbour)
+            for (neighbour in neighbours) {
+                if (!visited.contains(neighbour))
+                    connectedComponentLength += depthFirstConnectedComponentLength(graph, visited, neighbour)
             }
             return connectedComponentLength
 
         }
     }
+
     /**
      *
      */
-    fun calculateScore(player : Player): Int {
+    fun calculateScore(player: Player): Int {
         //ToDo
 
         onAllRefreshables { /*ToDo*/ }
@@ -76,7 +80,7 @@ class ScoringService(private val rootService : RootService) : AbstractRefreshing
     /**
      *
      */
-    private fun calculateLongestTerrain(type : Terrain, player : Player):Int {
+    private fun calculateLongestTerrain(type: Terrain, player: Player): Int {
         //ToDo
         return 0
     }
@@ -84,39 +88,41 @@ class ScoringService(private val rootService : RootService) : AbstractRefreshing
     /**
      *Calculates the Score resulted from the bear collection
      * @param player the player whose score should be calculated
+     * @return [Int] representing the score resulted from the Bear combinations of this player based on
+     * the current [entity.CascadiaGame.ruleSet]
      */
-    private fun calculateBearScore(player : Player): Int  {
-        val makeBearGraph : (Map<Pair<Int,Int>, HabitatTile>) -> Map<Pair<Int,Int>, List<Pair<Int,Int>>> ={
-                habitatTiles ->
-            val bearNodesCoordinates=  habitatTiles.filterValues { it.wildlifeToken?.animal == Animal.BEAR }
-                .keys.toSet()
+    private fun calculateBearScore(player: Player): Int {
+        val makeBearGraph: (Map<Pair<Int, Int>, HabitatTile>) -> Map<Pair<Int, Int>, List<Pair<Int, Int>>> =
+            { habitatTiles ->
+                val bearNodesCoordinates = habitatTiles.filterValues { it.wildlifeToken?.animal == Animal.BEAR }
+                    .keys.toSet()
 
-            val graph = bearNodesCoordinates.associateWith { coordinate ->
-                coordinate
-                    .neighbours()
-                    .filter { neighbour -> bearNodesCoordinates.contains(neighbour) }
+                val graph = bearNodesCoordinates.associateWith { coordinate ->
+                    coordinate
+                        .neighbours()
+                        .filter { neighbour -> bearNodesCoordinates.contains(neighbour) }
 
+                }
+                graph
             }
-            graph
-        }
         val bearGraph = makeBearGraph(player.habitat)
-        val visited : MutableSet<Pair<Int,Int>> = mutableSetOf()
-        val game = checkNotNull(rootService.currentGame){"No Game started yet!"}
+        val visited: MutableSet<Pair<Int, Int>> = mutableSetOf()
+        val game = checkNotNull(rootService.currentGame) { "No Game started yet!" }
         val isB = game.ruleSet[Animal.BEAR.ordinal]
         val searchedLength = if (isB) 3 else 2
         var connectedComponentsWithSearchedLength = 0
 
-        for(bearNode in bearGraph.keys){
-            if(!visited.contains(bearNode)) {
+        for (bearNode in bearGraph.keys) {
+            if (!visited.contains(bearNode)) {
                 if (depthFirstConnectedComponentLength(bearGraph, visited, bearNode) == searchedLength) {
                     connectedComponentsWithSearchedLength++
                 }
             }
         }
-        if(isB)
+        if (isB)
             return 10 * connectedComponentsWithSearchedLength
         else
-            return when (connectedComponentsWithSearchedLength){
+            return when (connectedComponentsWithSearchedLength) {
                 1 -> 4
                 2 -> 11
                 3 -> 19
@@ -128,7 +134,7 @@ class ScoringService(private val rootService : RootService) : AbstractRefreshing
     /**
      *
      */
-    private fun calculateElkScore(player : Player): Int {
+    private fun calculateElkScore(player: Player): Int {
         //ToDo
         return 0
     }
@@ -136,7 +142,7 @@ class ScoringService(private val rootService : RootService) : AbstractRefreshing
     /**
      *
      */
-    private fun calculateHawkScore(player : Player) : Int {
+    private fun calculateHawkScore(player: Player): Int {
         //ToDo
         return 0
     }
@@ -144,7 +150,7 @@ class ScoringService(private val rootService : RootService) : AbstractRefreshing
     /**
      *
      */
-    private fun calculateSalmonScore(player : Player): Int {
+    private fun calculateSalmonScore(player: Player): Int {
         //ToDo
         return 0
     }
@@ -152,7 +158,7 @@ class ScoringService(private val rootService : RootService) : AbstractRefreshing
     /**
      *
      */
-    private fun calculateFoxScore(player : Player): Int {
+    private fun calculateFoxScore(player: Player): Int {
         //ToDo
         return 0
     }
