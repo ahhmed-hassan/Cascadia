2252f68d4bcc9e5d5ad74d4c7d5bbe27853b9360
ahmed2.hassan <ahmed2.hassan@tu-dortmund.de>
2024-12-30 17:27:19 +0100

    Documentation

--- src/test/kotlin/service/LongestTerrainTester.kt
+++ src/test/kotlin/service/LongestTerrainTester.kt
@@ -8,32 +8,52 @@ import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+/**
+ * A testing class that validates the functionality of the method [ScoringService.calculateLongestTerrain]
+ * a player in the Cascadia board game. The tests include specific scenarios
+ * as outlined in the game's ruleset.
+ * @property player some dummy player to be shared among different tests
+ * @property rootService the [RootService] to access the [ScoringService]
+ */
 class LongestTerrainTester {
     val player: Player = Player("dummy", mutableMapOf(), PlayerType.NORMAL)
     val rootService = RootService()
-    private fun dummyTile(vararg terrainsArgs: Terrain): HabitatTile {
-        check(terrainsArgs.size == 6)
-        check(terrainsArgs.distinct().size in 1..2)
+
+    /**
+     * Helper function for make a dummy [HabitatTile] that focuses only on the right order of the terrains
+     * @param terrainsList the wished order of [Terrain]s
+     * */
+    private fun dummyTile(terrainsList: List<Terrain>): HabitatTile {
+        check(terrainsList.size == 6)
+        check(terrainsList.distinct().size in 1..2)
         return HabitatTile(
             id = Int.MAX_VALUE /*Not important*/,
             isKeystoneTile = false /*Not important*/,
             rotationOffset = Int.MAX_VALUE /*Not important*/,
             wildlifeSymbols = listOf() /*Not important*/,
             wildlifeToken = null /*Not important*/,
-            terrains = terrainsArgs.toList().toMutableList()
+            terrains = terrainsList.toMutableList()
         )
     }
 
-    private fun dummyTile(terrainsList: List<Terrain>) = dummyTile(*terrainsList.toTypedArray())
+    /**
+     * Enabling [Terrain] + List<[Terrain]> operation
+     */
     private operator fun Terrain.plus(other: List<Terrain>): MutableList<Terrain> =
         (mutableListOf(this) + other).toMutableList()
 
+    /**
+     * Enabling Terrain + Terrain
+     * @return [List] containing both terrains
+     */
     private operator fun Terrain.plus(other: Terrain): MutableList<Terrain> = mutableListOf(this, other)
 
-
+    /**
+     * Setting up the tiles as stated in page 9 of ruleset
+     */
     @BeforeTest
     fun setup() {
-        /*The setup environment in Cascadia Ruleset Page 9*/
+
         val tilesAndCoordinates: Map<Pair<Int, Int>, HabitatTile> = mapOf(
             (-2 to -1) to dummyTile(
                 Terrain.PRAIRIE + List(3) { Terrain.RIVER } + List(2) { Terrain.PRAIRIE }
@@ -114,27 +134,79 @@ class LongestTerrainTester {
         player.habitat.putAll(tilesAndCoordinates)
     }
 
+    /**
+     * Calculate the longest [Terrain.MOUNTAIN] path for [player] member
+     */
     @Test
-    fun testLongestPath() {
+    fun testLongestMountainPath() {
         val longestMountainPath = rootService.scoringService.calculateLongestTerrain(
             Terrain.MOUNTAIN, player
         )
+        assertEquals(6, longestMountainPath)
+    }
+
+    /**
+     *Calculate the longest [Terrain.FOREST] path for [player] member
+     */
+    @Test
+    fun testLongestForestPath() {
         val longestForestPath = rootService.scoringService.calculateLongestTerrain(
             Terrain.FOREST, player
         )
-        val longestWetlandPath = rootService.scoringService.calculateLongestTerrain(
-            Terrain.WETLAND, player
-        )
+        assertEquals(4, longestForestPath)
+    }
+
+    /**
+     *Calculate the longest [Terrain.PRAIRIE] path for [player] member
+     */
+    @Test
+    fun testLongestPrairiePath() {
         val longestPrairiePath = rootService.scoringService.calculateLongestTerrain(
             Terrain.PRAIRIE, player
         )
+        assertEquals(7, longestPrairiePath)
+    }
+
+    /**
+     *Calculate the longest [Terrain.RIVER] path for [player] member
+     */
+    @Test
+    fun testLongestRiverPath() {
         val longestRiverPath = rootService.scoringService.calculateLongestTerrain(
             Terrain.RIVER, player
         )
-        assertEquals(6, longestMountainPath)
-        assertEquals(4, longestForestPath)
-        assertEquals(8, longestWetlandPath)
-        assertEquals(7, longestPrairiePath)
         assertEquals(5, longestRiverPath)
+    }
+
+    /**
+     *Calculate the longest [Terrain.WETLAND] path for [player] member
+     */
+    @Test
+    fun testLongestWetlandPath() {
+        val longestWetlandPath = rootService.scoringService.calculateLongestTerrain(
+            Terrain.WETLAND, player
+        )
+        assertEquals(8, longestWetlandPath)
+    }
+
+    /**
+     * Checking that longest Wetland path for player with no Wet lands is 0
+     */
+    @Test
+    fun testNoWetLands() {
+        val player = Player("dummy",
+            mutableMapOf(
+                (0 to 0) to dummyTile(
+                    List(6) { Terrain.RIVER }
+                ),
+                (0 to 1) to dummyTile(
+                    List(6) { Terrain.MOUNTAIN }
+                ),
+                (0 to 3) to dummyTile(
+                    List(6) { Terrain.RIVER }
+                )
+            ),
+            PlayerType.NORMAL)
+        assertEquals(0, rootService.scoringService.calculateLongestTerrain(Terrain.WETLAND, player))
     }
 }
